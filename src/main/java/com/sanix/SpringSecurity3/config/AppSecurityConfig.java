41c5b12de2e30abb358ef9aaa1d5b8634528b08d
sanix <sanicksikani@gmail.com>
2020-10-04 22:44:40 +0300

    DAO Authentication

--- src/main/java/com/sanix/SpringSecurity3/config/AppSecurityConfig.java
+++ src/main/java/com/sanix/SpringSecurity3/config/AppSecurityConfig.java
@@ -1,12 +1,16 @@
 package com.sanix.SpringSecurity3.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.util.ArrayList;
@@ -16,6 +20,17 @@ import java.util.List;
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationProvider authProvider(){
+        DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return provider;
+    }
+    /*
     @Bean
     @Override
     protected UserDetailsService userDetailsService(){
@@ -23,4 +38,5 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
         users.add(User.withDefaultPasswordEncoder().username("navin").password("1234").roles("USER").build());
         return new InMemoryUserDetailsManager(users);
     }
+*/
 }
